4875c1fe18eda20c9104227961be29e400ab11ce
Fadi Asaad <firon1222@gmail.com>
2023-05-14 08:38:47 +0300

    update is_palindrome algorithm

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,33 +1,5 @@
 #include <stddef.h>
-#include <limits.h>
 #include "lists.h"
-
-/**
- * find_next_node - find the next occurrence of the value of the @node in range
- * @node: the starting node
- * @index: the starting index
- * @last_index: the index to stop before
- * Return: returns the node if any otherwise NULL
- */
-listint_t *find_next_node(listint_t *node, int *index, int last_index)
-{
-	int value = 0;
-
-	if (!node)
-		return (NULL);
-
-	value = node->n;
-	while (node->next && *index < last_index)
-	{
-		node = node->next;
-		(*index)++;
-		if (node->n == value)
-			break;
-	}
-	if (node->n == value)
-		return (node);
-	return (NULL);
-}
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -38,22 +10,24 @@ listint_t *find_next_node(listint_t *node, int *index, int last_index)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current_node = NULL, *last_node = NULL;
-	int current_index = 0, last_index = INT_MAX, index = 0;
+	listint_t *current_node = NULL, *searching_node = NULL, *last_node = NULL;
 
-	if (!head || !*head)
+	if (!head || !*head || !(*head)->next)
 		return (1);
-
 	current_node = *head;
-begin:
-	index = current_index;
-	last_node = find_next_node(current_node, &index, last_index);
-	if (current_index >= last_index)
+loop:
+	if (current_node == last_node)
 		return (1);
-	if (!last_node || index > last_index)
+	searching_node = current_node;
+	while (searching_node->next && searching_node != last_node)
+	{
+		searching_node = searching_node->next;
+		if (searching_node->n == current_node->n)
+			break;
+	}
+	if (searching_node->n != current_node->n)
 		return (0);
-	last_index = index;
-	current_index++;
+	last_node = searching_node;
 	current_node = current_node->next;
-	goto begin;
+	goto loop;
 }
