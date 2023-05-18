977073bdc3e7a2ac7624dc2f49ead00fa5cb0d02
Fadi Asaad <firon1222@gmail.com>
2023-05-18 06:03:10 +0300

    add task 103

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * get_pybytesobject_size - count the bytes inside the object
+ * @obj: the bytes object
+ */
+size_t get_pybytesobject_size(PyBytesObject *obj) {
+  char *data = obj->ob_sval;
+  size_t size = 0;
+
+	for (; *data; data++, size++)
+		;
+  return size;
+}
+
+/**
+ * print_pybytesobject_bytes - print first n bytes
+ * @obj: the bytes object
+ * @size: the number of bytes to print
+ */
+void print_pybytesobject_bytes(PyBytesObject *obj, Py_ssize_t size)
+{
+	char *data = obj->ob_sval;
+	Py_ssize_t i;
+
+	for (i = 0; i < size; i++)
+		printf("%02hx%c", data[i] & 0xff, i < size - 1 ? ' ' : 0);
+	putchar(10);
+}
+
+/**
+ * print_python_bytes - A c function to print the python object bytes
+ * @p: the python object
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, bytes_to_print;
+	PyBytesObject *obj = NULL;
+
+	if (!PyBytes_Check(p))
+	{
+		puts("  [ERROR] Invalid Bytes Object");
+		return;
+	}
+
+	obj = (PyBytesObject *)p;
+	size = get_pybytesobject_size(obj);
+	bytes_to_print = size + 1 >= 10 ? 10 : size + 1;
+	puts("[.] bytes object info");
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", PyBytes_AS_STRING(p));
+	printf("  first %ld bytes: ", bytes_to_print);
+	print_pybytesobject_bytes(obj, bytes_to_print);
+}
+
+/**
+ * print_python_list - a c function to print python list information
+ * @p: the python list object
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t i, size;
+
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < size; i++)
+	{
+		PyObject *item = PyList_GET_ITEM(p, i);
+
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		if (PyBytes_Check(p))
+			print_python_bytes(item);
+	}
+}
