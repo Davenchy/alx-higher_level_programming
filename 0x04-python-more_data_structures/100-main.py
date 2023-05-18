d46d11b72c5885cbbb03d22aa19775137f4e36b4
Fadi Asaad <firon1222@gmail.com>
2023-05-18 03:57:48 +0300

    update task 100

--- 0x04-python-more_data_structures/100-main.py
+++ 0x04-python-more_data_structures/100-main.py
@@ -5,4 +5,6 @@ my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
 # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
 result = weight_average(my_list)
 print("Average: {:0.2f}".format(result))
+result = weight_average()
+print("Average: {:0.2f}".format(result))
 
