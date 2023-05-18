69e17c0346f50498c135d0594df5f9bce0170dd7
Fadi Asaad <firon1222@gmail.com>
2023-05-18 12:59:16 +0300

    update task 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <Python.h>
 
@@ -12,24 +13,27 @@ void print_python_bytes(PyObject *p)
 
 	fflush(stdout);
 	puts("[.] bytes object info");
-	if (!PyBytes_Check(p))
+	if (!PyBytes_Check(p)) /* if not bytes object then print error */
 	{
 		puts("  [ERROR] Invalid Bytes Object");
 		return;
 	}
 
 	obj = (PyBytesObject *)p;
-	size = ((PyVarObject *)p)->ob_size;
+	size = ((PyVarObject *)p)->ob_size; /* get the used object size */
 
 	printf("  size: %ld\n", size);
-
-	if (++size > 10)
-		size = 10;
-	printf("  trying string: %s\n", obj->ob_sval);
-	printf("  first %ld bytes: ", size);
 	for (i = 0; i < size; i++)
-		printf("%02hx%c", obj->ob_sval[i] & 0xff, i < size - 1 ? ' ' : 0);
-	putchar(10);
+		if (!obj->ob_sval[i]) /* if end of string break */
+			break;
+		else if (isprint(obj->ob_sval[i])) /* print  character if printable */
+			putchar(obj->ob_sval[i]);
+	putchar(10); /* add new line */
+	if (++size > 10) /* recalculate the size to be less than 10 with NULL char */
+		size = 10;
+	printf("  first %ld bytes: ", size);
+	for (i = 0; i < size; i++) /* print each byte in formated hex with spaces */
+		printf("%02hhx%c", obj->ob_sval[i], i < size - 1 ? 32 : 10);
 }
 
 /**
@@ -43,20 +47,22 @@ void print_python_list(PyObject *p)
 
 	fflush(stdout);
 	puts("[*] Python list info");
-	if (!PyList_Check(p))
+	if (!PyList_Check(p)) /* if not a list object then stop execuation */
 		return;
 
 	obj = (PyListObject *)p;
-	size = ((PyVarObject *)p)->ob_size;
+	size = ((PyVarObject *)p)->ob_size; /* get the list used size */
 
 	printf("[*] Size of the Python List = %ld\n", size);
+	/* the allocated list size */
 	printf("[*] Allocated = %ld\n", obj->allocated);
 	for (i = 0; i < size; i++)
 	{
-		PyObject *item = obj->ob_item[i];
+		PyObject *item = obj->ob_item[i]; /* get item from the list */
 
+		/* the item's type name */
 		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
-		if (PyBytes_Check(item))
+		if (PyBytes_Check(item)) /* if item of type bytes the print info */
 			print_python_bytes(item);
 	}
 }
