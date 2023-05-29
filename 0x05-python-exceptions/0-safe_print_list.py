20b672d18c2cd02f6bc073b893c4e9231ce23143
Fadi Asaad <firon1222@gmail.com>
2023-05-29 21:03:43 +0300

    update task 0: apply pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
@@ -6,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
                 break
             print(val, end="")
             counter += 1
-    except:
+    except Exception:
         pass
     finally:
         print()
