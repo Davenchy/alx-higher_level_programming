a6063b56d9befb3bd8859e05ea1f824009c28725
Fadi Asaad <firon1222@gmail.com>
2023-05-31 01:09:30 +0300

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""A regualr comment like any comment"""
+
+class Square:
+    """Simple square class with no reason like my fucking life"""
+
+    def __init__(self, size) -> None:
+        self.__size = size
