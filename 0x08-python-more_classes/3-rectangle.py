60b82681f68a894065f54ace664ff56cee453b90
Fadi Asaad <firon1222@gmail.com>
2023-06-05 20:18:16 +0300

    update task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -63,6 +63,6 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        return "\n".join(
+        return "" if self.perimeter() == 0 else "\n".join(
             ["".join(["#" for _ in range(self.__width)])
                 for _ in range(self.__height)])
