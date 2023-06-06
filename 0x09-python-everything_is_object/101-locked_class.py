9ded3986085c6b401e5f5d9e9639587afd02e538
Fadi Asaad <firon1222@gmail.com>
2023-06-07 00:24:15 +0300

    update task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,7 +4,4 @@
 
 class LockedClass(object):
     """A locked class that only has first_name dynamic attribute"""
-    def __setattr__(self, key, value):
-        if key != "first_name":
-            raise AttributeError(f"object has no attribute '{key}'")
-        object.__setattr__(self, key, value)
+    __slots__ = ['first_name']
