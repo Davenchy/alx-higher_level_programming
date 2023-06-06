bd6aa43b1d7fc237418fceae473c5d6a54a578da
Fadi Asaad <firon1222@gmail.com>
2023-06-06 07:04:56 +0300

    add task 4: print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """prints a square using '#' char to the stdout
+    size (int): the size of a square size"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    [print("".join(["#" for _ in range(size)])) for _ in range(size)]
