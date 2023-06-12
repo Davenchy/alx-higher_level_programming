217b368be69d6f735ea30b7e6f8409b0d92e59c9
Fadi Asaad <firon1222@gmail.com>
2023-06-13 01:17:14 +0300

    add task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""model that contains function that detect if object is instance of a class"""
+
+def is_same_class(obj, a_class):
+    """checks if obj is instance of a_class"""
+    if obj is None:
+        return a_class is None
+    if not hasattr(obj, '__class__') or not hasattr(a_class, "__name__"):
+        return False
+    return obj.__class__.__name__ == a_class.__name__
