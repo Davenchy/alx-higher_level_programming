ae0fe128f28425ccbf1187944f58cedb420d3c57
Fadi Asaad <firon1222@gmail.com>
2023-06-13 02:21:59 +0300

    add task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A model that contains BaseGeometry class and subclasses"""
+
+
+class BaseGeometry:
+    """Geometry bass class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is integer and greater than 0"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """A Rectangle class based on the BaseGeometry class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
