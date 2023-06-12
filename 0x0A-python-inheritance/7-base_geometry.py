a0d4d9b68728174926aaaa34f9ab9aef74deb97c
Fadi Asaad <firon1222@gmail.com>
2023-06-13 02:07:29 +0300

    add task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Model that contains an empty class"""
+
+
+class BaseGeometry:
+    """Geometry bass class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is integer and greater than 0"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
