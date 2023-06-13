547bea6df206757ead68c601a546249c70b8fb34
Fadi Asaad <firon1222@gmail.com>
2023-06-14 01:46:23 +0300

    add task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A model that contains a function that convertes json object into string
+and writes it into a file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """converts ''my_obj'' into json string and write it into ''filename'' file
+    Args:
+        my_obj (object): object to convert
+        filename (str): the filename to write the json string into
+    """
+    json_str = json.dumps(my_obj)
+    written = 0
+    with open(filename, 'w') as f:
+        written = f.write(json_str)
+    return written
