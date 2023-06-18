b10f9bc7541faea5e0275f7daa5d733db0a07f23
Fadi Asaad <firon1222@gmail.com>
2023-06-18 12:58:18 +0300

    add task 3: Rectangle: validate setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -59,8 +59,8 @@ class Rectangle(Base):
         """Set the width value of the rectangle"""""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be greater than 0")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
@@ -68,8 +68,8 @@ class Rectangle(Base):
         """Set the height value of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be greater than 0")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
@@ -77,6 +77,8 @@ class Rectangle(Base):
         """Set the x-axis coordinate value of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
@@ -84,4 +86,6 @@ class Rectangle(Base):
         """Set the y-axis coordinate value of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,7 +11,7 @@ class TestRectangleCase(unittest.TestCase):
         self.assertTrue(hasattr(Rectangle, '__doc__'))
         self.assertTrue(hasattr(Rectangle.__init__, '__doc__'))
 
-        r = Rectangle(0, 0)
+        r = Rectangle(1, 1)
         self.assertIsInstance(r, Base)
         self.assertTrue(hasattr(r, '_Rectangle__width'))
         self.assertTrue(hasattr(r, '_Rectangle__height'))
@@ -20,56 +20,84 @@ class TestRectangleCase(unittest.TestCase):
         self.assertTrue(hasattr(r, 'id'))
 
     def test_width(self):
-        r = Rectangle(1, 0)
+        r = Rectangle(1, 2)
         self.assertEqual(r._Rectangle__width, 1)
 
-        r = Rectangle(10, 0)
+        r = Rectangle(10, 2)
         self.assertEqual(r.width, 10)
 
-        self.assertRaises(ValueError, Rectangle, -1, 0)
-        self.assertRaises(TypeError, Rectangle, "invalid", 0)
+        with self.assertRaises(ValueError) as err:
+            Rectangle(-1, 0)
+        self.assertEqual(str(err.exception), "width must be > 0")
+        with self.assertRaises(ValueError) as err:
+            Rectangle(0, 0)
+        self.assertEqual(str(err.exception), "width must be > 0")
+
+        with self.assertRaises(TypeError) as err:
+            Rectangle("invalid", 0)
+        self.assertEqual(str(err.exception), "width must be an integer")
 
     def test_height(self):
-        r = Rectangle(0, 1)
-        self.assertEqual(r._Rectangle__height, 1)
+        r = Rectangle(1, 2)
+        self.assertEqual(r._Rectangle__height, 2)
 
-        r = Rectangle(0, 10)
+        r = Rectangle(1, 10)
         self.assertEqual(r.height, 10)
 
-        self.assertRaises(ValueError, Rectangle, 0, -1)
-        self.assertRaises(TypeError, Rectangle, 0, "invalid")
+        with self.assertRaises(ValueError) as err:
+            Rectangle(1, -1)
+        self.assertEqual(str(err.exception), "height must be > 0")
+        with self.assertRaises(ValueError) as err:
+            Rectangle(1, 0)
+        self.assertEqual(str(err.exception), "height must be > 0")
+
+        with self.assertRaises(TypeError) as err:
+            Rectangle(1, "invalid")
+        self.assertEqual(str(err.exception), "height must be an integer")
 
     def test_x(self):
-        r = Rectangle(1, 0)
+        r = Rectangle(1, 2)
         self.assertEqual(r._Rectangle__x, 0)
 
-        r = Rectangle(1, 0, 5)
+        r = Rectangle(1, 2, 5)
         self.assertEqual(r._Rectangle__x, 5)
 
-        r = Rectangle(10, 0, 10)
-        self.assertEqual(r.x, 10)
+        r = Rectangle(1, 2, 5)
+        self.assertEqual(r.x, 5)
 
-        self.assertRaises(TypeError, Rectangle, 0, 0, "invalid")
+        with self.assertRaises(TypeError) as err:
+            Rectangle(1, 1, "invalid")
+        self.assertEqual(str(err.exception), "x must be an integer")
+
+        with self.assertRaises(ValueError) as err:
+            Rectangle(1, 1, -1)
+        self.assertEqual(str(err.exception), "x must be >= 0")
 
     def test_y(self):
-        r = Rectangle(0, 1)
+        r = Rectangle(1, 2)
         self.assertEqual(r._Rectangle__y, 0)
 
-        r = Rectangle(0, 1, 0, 5)
+        r = Rectangle(1, 2, 0, 5)
         self.assertEqual(r._Rectangle__y, 5)
 
-        r = Rectangle(0, 10, 10, 15)
+        r = Rectangle(1, 2, 10, 15)
         self.assertEqual(r.y, 15)
 
-        self.assertRaises(TypeError, Rectangle, 0, 0, 0, "invalid")
+        with self.assertRaises(TypeError) as err:
+            Rectangle(1, 1, 0, "invalid")
+        self.assertEqual(str(err.exception), "y must be an integer")
+
+        with self.assertRaises(ValueError) as err:
+            Rectangle(1, 1, 0, -1)
+        self.assertEqual(str(err.exception), "y must be >= 0")
 
     def test_id(self):
-        r = Rectangle(0, 0)
+        r = Rectangle(1, 2)
         self.assertEqual(r.id, 1)
 
-        r = Rectangle(0, 0, 0, 0)
+        r = Rectangle(1, 2, 0, 0)
         self.assertEqual(r.id, 2)
 
-        r = Rectangle(0, 0, 0, 0, 15)
+        r = Rectangle(1, 2, 0, 0, 15)
         self.assertEqual(r.id, 15)
         self.assertEqual(Base._Base__nb_objects, 2)
