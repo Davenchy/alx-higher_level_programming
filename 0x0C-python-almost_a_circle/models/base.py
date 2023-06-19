0b3e59707e81d22f63e8b780993f2ad905b4459a
Fadi Asaad <firon1222@gmail.com>
2023-06-19 04:11:29 +0300

    add task 16: Base: json string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    def to_dictionary(self):
+        """converts instance into a dictionary of its attributes"""
+        return self.__dict__
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Convert a list of dictionaries to a json string
@@ -36,3 +40,18 @@ class Base:
                 or not all(isinstance(i, dict) for i in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dicts")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes a list of objects that inherits from the Base class to a file
+        of the same name of the class name"""
+        objs = list_objs[:]
+        for i in range(len(objs)):
+            item = objs[i]
+            if isinstance(item, Base):
+                objs[i] = item.to_dictionary()
+            else:
+                raise TypeError(r"""list_objs items must be instances of
+                    classes inherits from Base""")
+        with open(cls.__name__ + ".json", "w") as file:
+            file.write(cls.to_json_string(objs))
