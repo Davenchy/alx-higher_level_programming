81066fbc5e3612ff0b97a05e733dc0b1b8804e4d
Fadi Asaad <firon1222@gmail.com>
2023-07-19 04:21:07 +0300

    update task 100: use alter table convert statment

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,4 @@
 -- sets database character set to utf88mb4 and collate to utf8mb4_unicode_ci
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- sets table character set to utf8mb4 and collate to utf8mb4_unicode_ci
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- sets name column character set to utf8mb4 and collate to utf8mb4_unicode_ci
-ALTER TABLE hbtn_0c_0.first_table MODIFY name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+-- converts table character set to utf8mb4 and collate to utf8mb4_unicode_ci
+ALTER TABLE hbtn_0c_0.first_table CONVERT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
