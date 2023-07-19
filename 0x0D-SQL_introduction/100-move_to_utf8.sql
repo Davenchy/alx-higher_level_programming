c64b99830a3891705f43315e67f6d9030ce401c5
Fadi Asaad <firon1222@gmail.com>
2023-07-19 04:23:03 +0300

    update task 100: FUCK MY LIFE!!!

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- sets database character set to utf88mb4 and collate to utf8mb4_unicode_ci
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- converts table character set to utf8mb4 and collate to utf8mb4_unicode_ci
-ALTER TABLE hbtn_0c_0.first_table CONVERT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
