c0d41a0393deda0e4e896558f72c2d0fe93c6580
Fadi Asaad <firon1222@gmail.com>
2023-07-20 02:23:21 +0300

    Fuck!!!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists the privileges of the users user_0d_1 and user_0d_2 on your server
+-- I hate red check boxes without reasons
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
