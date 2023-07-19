7c73c3b4a483b79f8f47d6f5e9ff15dba82c9b3f
Fadi Asaad <firon1222@gmail.com>
2023-07-20 02:21:04 +0300

    update task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- Lists the privileges of the users user_0d_1 and user_0d_2 on your server
--- Lists the privileges of the users user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Lists the privileges of the users user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
