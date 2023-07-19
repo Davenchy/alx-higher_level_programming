1fe1abd93c6a6ebcf85a35838fbe543b0018013a
Fadi Asaad <firon1222@gmail.com>
2023-07-20 01:59:58 +0300

    update task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
+-- Displays the privileges of the MySQL users user_0d_1 and user_0d_2
 -- Displays the privileges granted to the user user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- Displays the privileges granted to the user user_0d_2
