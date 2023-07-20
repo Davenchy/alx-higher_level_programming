fc2dbe78b66851b54a93ee94253ef12d36a16fc7
Fadi Asaad <firon1222@gmail.com>
2023-07-20 03:03:38 +0300

    add task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create hbtn_0d_usa database only if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create states table only if not exists with id as a primary key and name columns
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL
+), FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id);
