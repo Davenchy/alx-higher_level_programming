fa23646abf88f256b38afbe83b134eedba22c057
Fadi Asaad <firon1222@gmail.com>
2023-07-20 04:00:46 +0300

    add task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- List all shows that have at least one genre linked
+SELECT s.title, g.id
+FROM tv_show_genres sg
+	JOIN tv_shows s
+	JOIN tv_genres g
+WHERE sg.show_id = s.id AND sg.genre_id = g.id
+ORDER BY s.title, g.id;
