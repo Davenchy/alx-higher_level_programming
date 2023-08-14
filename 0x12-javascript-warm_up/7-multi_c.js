955da664b29225ba1ae7bd36d5efee4c9feded63
Fadi Asaad <firon1222@gmail.com>
2023-08-14 22:02:00 +0300

    add task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+if (x) {
+  for (let i = 0; i < x; i++) {
+    console.log('c is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
