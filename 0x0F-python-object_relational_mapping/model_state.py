d5df559c74c559e89b1ac1594ab558e2e9d3c294
Fadi Asaad <firon1222@gmail.com>
2023-08-18 15:51:46 +0300

    add task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""The State class definition for the ORM"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """The State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
