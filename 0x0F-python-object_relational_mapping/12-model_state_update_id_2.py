7f24ea738b16fa7b00cd3f805a5ee3995e2cf354
Fadi Asaad <firon1222@gmail.com>
2023-08-18 20:29:21 +0300

    add task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""rename a state of id 2 to New Mexico 
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    state = session.query(State).get(2)
+    if state is not None:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
