10d764f634e1d29450243dce0e1788242fa5174f
Fadi Asaad <firon1222@gmail.com>
2023-09-26 17:53:07 +0300

    update task 4: redo the task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 
-const request = require('request');
-const BASE_URL = 'https://swapi-api.alx-tools.com/api';
+const request = require("request");
+const baseUrl = process.argv[2];
 const userId = 18;
 
-request(`${BASE_URL}/people/${userId}`, (error, _, body) => {
+request(baseUrl, (error, _, body) => {
   if (error) {
     console.log(error);
     process.exit();
   }
 
   const data = JSON.parse(body);
-  console.log(data.films.length);
+  const count = data.results
+    .map((f) => f.characters)
+    .filter((c) => c.some(c => c.includes(userId))).length;
+  console.log(count);
 });
